97ad225074e8b42c0b1b23ef2a1a95d51786ba52
Macaarena <142362434+Vanesa1212@users.noreply.github.com>
2024-07-26 22:11:41 -0500

    realizacion del login etc

---
+++ app/src/main/java/com/sena/assiscor/Recuperar_contras.kt
@@ -0,0 +1,20 @@
+package com.sena.assiscor
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class Recuperar_contras : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_recuperar_contras)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
